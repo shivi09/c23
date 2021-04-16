af2f36cb78084eea3d45b8a32a8fb3dfa9231d76
shivi09 <jainshivu09@gmail.com>
2021-04-16 11:35:20 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,12 @@
+class Box {
+    constructor(x,y, width, height){
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x,y , width, height);
+        World.add(world, this.body);
+    }
+    display(){
+        rectMode(CENTER);
+        rect(this.body.position.x, this.body.position.y,this.width,this.height);
+    }
+}
